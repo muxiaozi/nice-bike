ab8e4e58285d90f1f9fe05e37ee17c97f673dc2b
weiyastory <m18749679912@163.com>
2018-06-05 21:49:07 +0800

    前端

---
+++ pages/Record/record.js
@@ -0,0 +1,65 @@
+var amapFile = require('../../libs/amap-wx.js'); 
+var markersData = [];
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    textData: {}
+  },
+  makertap: function (e) {
+    var id = e.markerId;
+    var that = this;
+    that.showMarkerInfo(markersData, id);
+    that.changeMarkerColor(markersData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: '3511d4db187cf871453767e4aea419c9' });
+    myAmapFun.getPoiAround({
+      iconPathSelected: '../../static/map_icon/marker_active.png',  
+      iconPath: '../../static/map_icon/marker_normal.png',  
+      success: function (data) {
+        markersData = data.markers;
+        that.setData({
+          markers: markersData
+        });
+        that.setData({
+          latitude: markersData[0].latitude
+        });
+        that.setData({
+          longitude: markersData[0].longitude
+        });
+        that.showMarkerInfo(markersData, 0);
+      },
+      fail: function (info) {
+        wx.showModal({ title: info.errMsg })
+      }
+    })
+  },
+  showMarkerInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      textData: {
+        name: data[i].name,
+        desc: data[i].address
+      }
+    });
+  },
+  changeMarkerColor: function (data, i) {
+    var that = this;
+    var markers = [];
+    for (var j = 0; j < data.length; j++) {
+      if (j == i) {
+        data[j].iconPath = '../../static/map_icon/marker_active.png'; 
+      } else {
+        data[j].iconPath = '../../static/map_icon/marker_normal.png';  
+      }
+      markers.push(data[j]);
+    }
+    that.setData({
+      markers: markers
+    });
+  }
+
+})
