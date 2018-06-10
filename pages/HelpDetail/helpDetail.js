77b5228e0cec7817cf4858021bee9daaa3866198
weiyastory <m18749679912@163.com>
2018-06-10 22:47:53 +0800

    bicycle

---
+++ pages/HelpDetail/helpDetail.js
@@ -0,0 +1,12 @@
+const app = getApp()
+
+Page({
+  data:{
+    id: '1',         //帮助ID
+    title: '为什么加入公益单车',      //帮助题目
+    content: '因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个因为这个因为那个',    //解决办法 文字描述
+    images: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528629326000&di=a0e9a85c96bb44b4bb54300d36378e95&imgtype=0&src=http%3A%2F%2F58pic.ooopic.com%2F58pic%2F13%2F01%2F20%2F66258PICYfn.jpg',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1528619272&di=6cb660c5be2be02eb93a353a981bbdf3&src=http://pic39.nipic.com/20140317/4499633_211613049395_2.jpg',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1529224109&di=5274a6114bb231600fd8019b33ce33e1&imgtype=jpg&er=1&src=http%3A%2F%2Fpic39.nipic.com%2F20140314%2F4499633_220605003350_2.jpg']
+  }
+})
